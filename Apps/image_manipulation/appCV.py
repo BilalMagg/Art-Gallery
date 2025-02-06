b4c98e554cbacb3f0800d0bc39fa625a2fbdc5e1
Bilal <gygcjg55@gmail.com>
2025-02-06 23:32:24 +0100

    Updated image templates and cleaned up images

--- Apps/image_manipulation/appCV.py
+++ Apps/image_manipulation/appCV.py
@@ -8,8 +8,8 @@ import numpy as np
 # app = Flask(__name__)
 image_bp = Blueprint("images", __name__, template_folder="templates",static_folder="static")
 
-os.makedirs('static/images',exist_ok= True)
-os.makedirs('static/filtered',exist_ok= True)
+os.makedirs('Apps/image_manipulation/static/images',exist_ok= True)
+os.makedirs('Apps/image_manipulation/static/filtered',exist_ok= True)
 
 @image_bp.route('/')
 def index():
@@ -20,7 +20,7 @@ def upload_image():
   if 'image' in request.files:
         img_file = request.files['image']
         if img_file:
-            file_path = os.path.join('static/images', img_file.filename)
+            file_path = os.path.join('Apps/image_manipulation/static/images', img_file.filename)
             img_file.save(file_path)
             return redirect(url_for('images.show_image', filename=img_file.filename))
   return redirect(url_for('index'))
@@ -31,8 +31,8 @@ def show_image(filename):
 
 @image_bp.route('/apply_filter/<filter_type>/<filename>')
 def apply_filter(filter_type,filename):
-    input_path = os.path.join('static/images',filename)
-    output_path = os.path.join('static/filtered',f"{filter_type}_{filename}")
+    input_path = os.path.join('Apps/image_manipulation/static/images',filename)
+    output_path = os.path.join('Apps/image_manipulation/static/filtered',f"{filter_type}_{filename}")
 
     if not os.path.exists(input_path):
         return "File Not Found!!",404
@@ -60,13 +60,15 @@ def apply_filter(filter_type,filename):
         return "Invalid filter",400
     cv2.imwrite(output_path,img)
 
-    return render_template('show_imageCV.html',filename=filename,filtered_filename=f"filtered/{filter_type}_{filename}")
+    return render_template('show_imageCV.html',filename=filename,filtered_filename=f"{filter_type}_{filename}")
 
 @image_bp.route('/download/<filename>')
 def download_img(filename):
-    file_path = os.path.join('static/filtered',filename)
+    file_path = os.path.join('Apps/image_manipulation/static/filtered',filename)
+    print (file_path)
 
     if not os.path.exists(file_path):
+        print('Not found')
         return "File Not Found !!",404
     
     return send_file(file_path, as_attachment = True)
