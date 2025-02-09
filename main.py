b2a9e85e700c7d769a6c2dc37dcbe83af1d0d41a
Bilal <gygcjg55@gmail.com>
2025-02-09 20:17:13 +0100

    Linking the data page to the home

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 # Import blueprints from each app
 from Apps.paint_tool.app import shapes_bp
-# from apps.data_visualization.app import data_bp
+from Apps.data_visualization.app import data_bp
 from Apps.image_manipulation.appCV import image_bp
 from Apps.audio_manipulation.app import audio_bp
 # from apps.machine_learning.app import ml_bp
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 # Register blueprints with URLs
 app.register_blueprint(shapes_bp, url_prefix="/shapes")
-# app.register_blueprint(data_bp, url_prefix="/visualization")
+app.register_blueprint(data_bp, url_prefix="/visualization")
 app.register_blueprint(image_bp, url_prefix="/images")
 app.register_blueprint(audio_bp, url_prefix="/audios")
 # app.register_blueprint(ml_bp, url_prefix="/ml")
